b1c600d2a52c706c5f6742603bf428905d8410d4
Anguh Bleaise <bleaise.anguh.quigo@gmail.com>
2024-04-30 18:16:39 +0100

    Assignemtn and Discussion. Unit 1 2 and 3 updated

---
+++ PYTHON-CS 1101-01/Discussion Unit 2.py
@@ -0,0 +1,21 @@
+def discussion_unit(answer):  # function that takes a parameter called answer
+    print(answer)
+
+
+# defined variable
+my_country = 'Cameroon'
+
+# defined variable of current month April(4th month)
+current_month = 4
+
+# function call printing a value
+discussion_unit('Yes, this function call works')
+
+# function call printing a variable
+discussion_unit(my_country)
+
+# function call printing an expression
+discussion_unit('Just in case you\'re wondering, I am from ' + my_country)
+
+# function call printing an expression about my country multiplied by month
+discussion_unit(current_month * my_country)
